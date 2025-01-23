135e72765257319556a68f903b8256f41c667d4f
unknown <mukaberthe12@gmail.com>
2025-01-23 11:09:53 +0200

    Welcome to new staff MIS

--- index.php
+++ index.php
@@ -3,9 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>New Staff MIS</title>
 </head>
 <body>
-    
+    <h1>Welcome to new staff MIS</h1>
+    <a href="registration.php">Go to Registration Page</a>
 </body>
 </html>
